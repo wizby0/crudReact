dfc7997ab37442673d47fcc3c63c56dfbdf4fbbe
wizby0@naver.com <>
2019-02-15 13:40:03 +0900

    add edit function added

--- jugtours/app/src/App.js
+++ jugtours/app/src/App.js
@@ -1,41 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import GroupList from './GroupList';
+import GroupEdit from './GroupEdit';
 
 class App extends Component {
-    state = {
-        isLoading: true,
-        groups: []
-    };
-
-    async componentDidMount() {
-        const response = await fetch('/api/groups');
-        const body = await response.json();
-        this.setState({ groups: body, isLoading: false });
-    }
-
     render() {
-        const {groups, isLoading} = this.state;
-
-        if (isLoading) {
-            return <p>Loading...</p>;
-        }
-
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <div className="App-intro">
-                        <h2>JUG List</h2>
-                        {groups.map(group =>
-                            <div key={group.id}>
-                                {group.name}
-                            </div>
-                        )}
-                    </div>
-                </header>
-            </div>
-        );
+            <Router>
+                <Switch>
+                    <Route path='/' exact={true} component={Home}/>
+                    <Route path='/groups' exact={true} component={GroupList}/>
+                    <Route path='/groups/:id' component={GroupEdit}/>
+                </Switch>
+            </Router>
+        )
     }
 }
 
